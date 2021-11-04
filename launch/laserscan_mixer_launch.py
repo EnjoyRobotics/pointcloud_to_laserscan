236868a62c7bfbdaa8dc008f90a5a32c0900a339
Feras Terkawi <44900316+ferasterkawi@users.noreply.github.com>
2021-11-04 12:35:33 +0100

    Update laserscan_mixer_launch.py

--- launch/laserscan_mixer_launch.py
+++ launch/laserscan_mixer_launch.py
@@ -20,7 +20,7 @@ def generate_launch_description():
             name='laserscan_to_pointcloud',
             remappings=[('scan_in', 'lidar1/scan'),
                         ('cloud', [LaunchConfiguration(variable_name='scanner'), '/cloud1'])],
-            parameters=[{'target_frame': 'base_link', 'transform_tolerance': 0.01}]
+            parameters=[{'target_frame': 'lidar_link', 'transform_tolerance': 0.01}]
         ),
         Node(
             package='laserscan_mixer',
@@ -28,7 +28,7 @@ def generate_launch_description():
             name='laserscan_to_pointcloud',
             remappings=[('scan_in', 'lidar2/scan'),
                         ('cloud', [LaunchConfiguration(variable_name='scanner'), '/cloud2'])],
-            parameters=[{'target_frame': 'base_link', 'transform_tolerance': 0.01}]
+            parameters=[{'target_frame': 'lidar_link', 'transform_tolerance': 0.01}]
         ),
         Node(
             package='laserscan_mixer',
@@ -40,13 +40,13 @@ def generate_launch_description():
             remappings=[('cloud_in', '/cloud'),
                         ('scan', [LaunchConfiguration(variable_name='scanner'), '/scan'])],
             parameters=[{
-                'target_frame': 'lidar2',
+                'target_frame': 'lidar_link',
                 'transform_tolerance': 0.01,
                 'min_height': 0.0,
                 'max_height': 1.0,
                 'angle_min': -1.5708*2,  # -M_PI/2
                 'angle_max': 1.5708*2,  # M_PI/2
-                'angle_increment': 0.0087,  # M_PI/360.0
+                'angle_increment': 0.0087,  # M_PI/360
                 'scan_time': 0.3333,
                 'range_min': 0.01,
                 'range_max': 64.0,
