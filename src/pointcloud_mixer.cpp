fc16dad2e253ea2f2e91618b438ea8ca6774afa9
Feras Terkawi <44900316+ferasterkawi@users.noreply.github.com>
2021-11-04 12:27:26 +0100

    Update pointcloud_mixer.cpp

--- src/pointcloud_mixer.cpp
+++ src/pointcloud_mixer.cpp
@@ -1,91 +1,90 @@
 /*
- *  Two YDLIDARs Mixer
+ *  Two YDLIDARs Mixer V2.0
  *  Enjoy Robotics
  */
+
+#include <chrono>
 
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
 #include "sensor_msgs/msg/point_cloud2.hpp"
 
-sensor_msgs::msg::LaserScan scan_msg;
-sensor_msgs::msg::PointCloud2 cloud_msg;
+using std::placeholders::_1;
+using namespace std::chrono_literals;
 
-std::vector<uint8_t> cloud1;
-std::vector<uint8_t> cloud2;
-std::vector<uint8_t> cloud;
-
-uint32_t width1;
-uint32_t width2;
-
-int size;
-
-void pointcloudCb1 (const sensor_msgs::msg::PointCloud2::SharedPtr point_cloud2_msgs)
+class cloud_Mixer : public rclcpp::Node
 {
-  cloud1 = point_cloud2_msgs->data;
-  width1 = point_cloud2_msgs->width;
+    public:
+    cloud_Mixer()
+    : Node("pointcloud_mixer")
+        {
+            lidar_info_sub1_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+                    "/scanner/cloud1", rclcpp::SensorDataQoS(), std::bind(&cloud_Mixer::pointcloudCb1, this, _1));
+            
+            lidar_info_sub2_ = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+                    "/scanner/cloud2", rclcpp::SensorDataQoS(), std::bind(&cloud_Mixer::pointcloudCb2, this, _1));
 
-  // printf("[YDLIDAR INFO1]: data_size-point_step : [%ld, %d]\n",  cloud1.size(), cloud1[0]);
-}
+            pointcloud_pub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>("cloud", rclcpp::SensorDataQoS());
 
-void pointcloudCb2 (const sensor_msgs::msg::PointCloud2::SharedPtr point_cloud2_msgs)
+            timer_ = this->create_wall_timer(
+                    20ms, std::bind(&cloud_Mixer::pointcloudCb, this));
+
+        }
+        sensor_msgs::msg::PointCloud2 cloud_msg;
+
+        std::vector<uint8_t> cloud1;
+        std::vector<uint8_t> cloud2;
+        std::vector<uint8_t> cloud;
+        uint32_t width1;
+        uint32_t width2;
+        int size;
+
+    private:
+        void pointcloudCb1 (const sensor_msgs::msg::PointCloud2::SharedPtr point_cloud2_msgs)
+        {
+            cloud1 = point_cloud2_msgs->data;
+            width1 = point_cloud2_msgs->width;
+        }
+        void pointcloudCb2 (const sensor_msgs::msg::PointCloud2::SharedPtr point_cloud2_msgs)
+        {
+            
+            cloud2 = point_cloud2_msgs->data;
+            width2 = point_cloud2_msgs->width;
+            
+            cloud_msg.header.stamp.sec = point_cloud2_msgs -> header.stamp.sec;
+            cloud_msg.header.stamp.nanosec = point_cloud2_msgs->header.stamp.nanosec;
+            cloud_msg.header.frame_id = point_cloud2_msgs->header.frame_id ;
+            cloud_msg.fields = point_cloud2_msgs->fields;
+            cloud_msg.height = point_cloud2_msgs->height;
+            cloud_msg.is_bigendian = point_cloud2_msgs->is_bigendian;
+            cloud_msg.width = width1 + width2;
+            cloud_msg.point_step = point_cloud2_msgs -> point_step;
+            cloud_msg.row_step = point_cloud2_msgs->row_step;
+            cloud_msg.is_dense = point_cloud2_msgs->is_dense;
+
+            cloud.erase(cloud.begin(),cloud.end());
+
+            cloud.reserve(cloud.size() + cloud1.size());
+            cloud.insert(cloud.end(), cloud1.begin(), cloud1.end());
+            cloud.reserve(cloud.size() + cloud2.size());
+            cloud.insert(cloud.end(), cloud2.begin(), cloud2.end());
+
+            cloud_msg.data = cloud;
+        }
+        void pointcloudCb()
+        {
+            pointcloud_pub_->publish(std::move(cloud_msg));
+        }
+        rclcpp::TimerBase::SharedPtr timer_;
+        rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr pointcloud_pub_;
+        rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr lidar_info_sub1_;
+        rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr lidar_info_sub2_;
+};
+
+int main(int argc, char * argv[])
 {
-  cloud2 = point_cloud2_msgs->data;
-  width2 = point_cloud2_msgs->width;
-
-  cloud_msg.header.stamp.sec = point_cloud2_msgs -> header.stamp.sec;
-  cloud_msg.header.stamp.nanosec = point_cloud2_msgs->header.stamp.nanosec;
-  cloud_msg.header.frame_id = point_cloud2_msgs->header.frame_id;
-  cloud_msg.fields = point_cloud2_msgs->fields;
-  cloud_msg.height = point_cloud2_msgs->height;
-  cloud_msg.is_bigendian = point_cloud2_msgs->is_bigendian;
-  cloud_msg.width = width1 + width2;
-  cloud_msg.point_step = point_cloud2_msgs -> point_step;
-  cloud_msg.row_step = point_cloud2_msgs->row_step;
-  cloud_msg.is_dense = point_cloud2_msgs->is_dense;
-
-  // printf("[YDLIDAR INFO2]: data_size-point_step : [%ld, %d]\n",  cloud2.size(), cloud2[0]);
-
-  cloud.erase(cloud.begin(),cloud.end());
-
-  cloud.reserve(cloud.size() + cloud1.size());
-  cloud.insert(cloud.end(), cloud1.begin(), cloud1.end());
-  cloud.reserve(cloud.size() + cloud2.size());
-  cloud.insert(cloud.end(), cloud2.begin(), cloud2.end());
-
-  // printf("[YDLIDAR INFO]: data_size-point_step : [%ld, %d]\n",  cloud.size(), cloud[0]);
-  cloud_msg.data = cloud;
-}
-
-int main(int argc, char **argv) {
   rclcpp::init(argc, argv);
-
-  auto node = rclcpp::Node::make_shared("ydlidar_ros2_driver_client");
-
-  auto lidar_info_sub1 = node->create_subscription<sensor_msgs::msg::PointCloud2>(
-                        "/scanner/cloud1", rclcpp::SensorDataQoS(), pointcloudCb1);
-
-  auto lidar_info_sub2 = node->create_subscription<sensor_msgs::msg::PointCloud2>(
-                        "/scanner/cloud2", rclcpp::SensorDataQoS(), pointcloudCb2);
-
-  // auto laser_pub = node->create_publisher<sensor_msgs::msg::LaserScan>("scan", rclcpp::SensorDataQoS());
-
-  auto pointcloud_pub = node->create_publisher<sensor_msgs::msg::PointCloud2>("cloud", rclcpp::SensorDataQoS());
-
-  rclcpp::WallRate loop_rate(20);
-
-  while (rclcpp::ok()) {
-
-    pointcloud_pub->publish(cloud_msg);
-
-    if(!rclcpp::ok()) {
-      break;
-    }
-    rclcpp::spin_some(node);
-    loop_rate.sleep();
-  }
-  
-  rclcpp::spin(node);
+  rclcpp::spin(std::make_shared<cloud_Mixer>());
   rclcpp::shutdown();
-  
   return 0;
 }
